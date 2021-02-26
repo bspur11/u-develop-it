a5c7424c79cfdf630cb8ce3098c98929658e62df
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-25 21:55:23 -0500

    added party

--- server.js
+++ server.js
@@ -33,8 +33,12 @@ app.get('/', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates 
-               WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   //  Because params can be accepted in the database call as an array, params is assigned as an array with a single element, req.params.id.12.2.5
   const params = [req.params.id];
   // using the Database method get() to return a single row from the database call 12.2.5
@@ -59,9 +63,11 @@ app.get('/api/candidate/:id', (req, res) => {
 app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
-  db.run(sql, params, function(err, result) {
+  db.run(sql, params, function (err, result) {
     if (err) {
-      res.status(400).json({ error: res.message });
+      res.status(400).json({
+        error: res.message
+      });
       return;
     }
 
@@ -74,32 +80,38 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 // use the HTTP request method post() to insert a candidate into the candidates table12.2.7
-app.post('/api/candidate', ({ body }, res) => {
+app.post('/api/candidate', ({
+  body
+}, res) => {
   //  ***Notice that we're using object destructuring to pull the body property out of the request object.***
   const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
   if (errors) {
-    res.status(400).json({ error: errors });
+    res.status(400).json({
+      error: errors
+    });
     return;
   }
   //  database call 12.2.7
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
   VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
-// ES5 function, not arrow function, to use `this`
-//  no column for the id. SQLite will autogenerate the id 12.2.7
-db.run(sql, params, function(err, result) {
-if (err) {
-  // 400 status code, to prompt for a different user request
-res.status(400).json({ error: err.message });
-return;
-}
-//  send the response using the res.json() method with this.lastID, the id of the inserted row 12.2.7
-res.json({
-message: 'success',
-data: body,
-id: this.lastID
-});
-});
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use `this`
+  //  no column for the id. SQLite will autogenerate the id 12.2.7
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      // 400 status code, to prompt for a different user request
+      res.status(400).json({
+        error: err.message
+      });
+      return;
+    }
+    //  send the response using the res.json() method with this.lastID, the id of the inserted row 12.2.7
+    res.json({
+      message: 'success',
+      data: body,
+      id: this.lastID
+    });
+  });
 
 });
 
@@ -107,7 +119,11 @@ id: this.lastID
 
 // Get all candidates the api in the URL signifies that this is an API endpoint.
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+              AS party_name 
+              FROM candidates 
+              LEFT JOIN parties 
+              ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -150,3 +166,5 @@ db.on('open', () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+// http://localhost:3001/api/candidates
